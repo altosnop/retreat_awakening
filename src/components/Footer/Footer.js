5510523d58c4653eeedce41e49841e6888f1be94
Alex Lar <shimanuchi@gmail.com>
2021-09-16 22:31:07 +0300

    footer fix

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,17 +1,35 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
-    return (
-        <footer className='footer'>
-            <p>© 2021 — Все права защищены.</p>
-            <div className='footer_contacts'>
-                <a target="_blank" rel="noreferrer" href='https://t.me/margo_khostikyan'>Марго</a>
-                <span className="footer_line">│</span>
-                <a target="_blank" rel="noreferrer" href='https://t.me/natalia_larchenko'>Наталия</a>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="footer">
+      <p>© 2021 — Все права защищены.</p>
+      <div className="footer_contacts">
+        <div className='footer_contact_wrapper'>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://t.me/margo_khostikyan"
+          >
+            Telegram Марго
+          </a>
+          <a href='tel:+79037779278'>+79037779278</a>
+        </div>
+        <span className="footer_line">│</span>
+        <div className='footer_contact_wrapper'>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://t.me/natalia_larchenko"
+          >
+            Telegram Наталия
+          </a>
+          <a href='tel:+380986478787'>+380986478787</a>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
